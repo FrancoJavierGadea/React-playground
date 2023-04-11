e576aece33c634bd29f5222db9f56eae49f553fb
1812 <francogadea1812@gmail.com>
2023-04-10 23:53:24 -0300

    Agregando carpeta examples

---
+++ examples/default/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+//Components
+
+
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);

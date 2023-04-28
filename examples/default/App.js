79d547ffecb0f197713889c736816dddaa0ff2b3
1812 <francogadea1812@gmail.com>
2023-04-28 17:35:13 -0300

    actualizando examples

--- examples/default/App.js
+++ examples/default/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 
 function App(){
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount(old => ++old);
+
+    const decrement = () => setCount(old => --old);
     
     return (<div>
 
-        <h1>! Hello React !</h1>
+        <h1 className="text-center">Hello React</h1>
         
+        <h2 className="text-center">{count}</h2>
+
+        <div className="d-flex justify-content-center">
+            <button className="btn btn-primary mx-2" onClick={increment}></button>
+            <button className="btn btn-primary mx-2" onClick={decrement}></button>
+        </div>
+
     </div>);
 }

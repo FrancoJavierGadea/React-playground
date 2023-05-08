8eec733849efb618c8d98d96ff05e12612b09b57
1812 <francogadea1812@gmail.com>
2023-05-08 20:37:41 -0300

    agregando imagen preview

--- src/utils/github.js
+++ src/utils/github.js
@@ -26,47 +26,60 @@ function getLanguage(fileName = ''){
 
 async function getList(URL){
 
-    const response = await fetch(URL);
+    try {
+        
+        const response = await fetch(URL);
+    
+        const value = await response.json();
+    
+        const folders = value
+            .filter(({type}) => type === 'dir')
+            .map(({name, html_url}) => ({name, source: html_url}));
+        
+        return folders;   
+    }
+    catch (error) {
+        
+    }
 
-    const value = await response.json();
-
-    console.log(value);
-
-    const folders = value
-        .filter(({type}) => type === 'dir')
-        .map(({name, html_url}) => ({name, source: html_url}));
-
-    return folders;   
+    return [];
 }
 
 async function download(URL, name = 'default'){
 
-    const response = await fetch(`${URL}/${name}`);
+    try {
+        
+        const response = await fetch(`${URL}/${name}`);
+    
+        const value = await response.json();
+    
+        const files = value.filter(({type}) => type === 'file');
+    
+        const project = await Promise.all(files.map(async file => {
+    
+            const {name, download_url} = file;
+    
+            const value = await (await fetch(download_url)).text();
+    
+            return {
+                name: `/${name}`,
+                language: getLanguage(name),
+                value
+            }
+        }));
+    
+        return project.reduce((acc, value) => {
+    
+            acc[value.name] = value;
+    
+            return acc;
+        }, {});
+    }
+    catch (error) {
 
-    const value = await response.json();
+    }
 
-    const files = value.filter(({type}) => type === 'file');
-
-
-    const project = await Promise.all(files.map(async file => {
-
-        const {name, download_url} = file;
-
-        const value = await (await fetch(download_url)).text();
-
-        return {
-            name: `/${name}`,
-            language: getLanguage(name),
-            value
-        }
-    }));
-
-    return project.reduce((acc, value) => {
-
-        acc[value.name] = value;
-
-        return acc;
-    }, {});
+    return {};
 }
 
 
